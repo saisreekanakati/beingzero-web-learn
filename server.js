9ae1e18efd185452202a9a8407c6cc954684233f
Sai Sree Kanakati <saisreekanakati@gmail.com>
2021-04-30 12:17:12 +0530

    my resume

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 
 const app = express();
+app.use(express.static(__dirname+"/frontend"));
 
 app.get("/", function(req, res){
     res.send("Welcome to saisree's Site");
 });
 app.get("/resume", function(req, res){
     file=__dirname+"/resume.html";
+    res.sendFile(file);
+});
+app.get("/resumee", function(req, res){
+    file=__dirname+"/frontend/html/resume.html";
     res.sendFile(file);
 })
 
