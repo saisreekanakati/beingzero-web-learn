2f558f1213e263181a78a2fd1acd76a9edf1ed24
Sai Sree Kanakati <saisreekanakati@gmail.com>
2021-05-19 23:44:11 +0530

    lo

--- server.js
+++ server.js
@@ -73,9 +73,9 @@ app.get("/register", function(req, res){
 })
 
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
  
 // Start the server
-app.listen(PORT, function(){
-    console.log("runnig server http://localhost:"+PORT);
+app.listen(port, function(){
+    console.log("runnig server http://localhost:"+port);
 })
