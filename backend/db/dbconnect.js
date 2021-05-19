6719239bb4b58a28a84224781487d1129d493c9d
Sai Sree Kanakati <saisreekanakati@gmail.com>
2021-05-19 23:13:04 +0530

    lsd

--- backend/db/dbconnect.js
+++ backend/db/dbconnect.js
@@ -4,7 +4,7 @@ module.exports.connect = function(){
     var p=process.env.Mongo_atlas_password;
     var connectionString = "mongodb+srv://saisreekanakati:"+p+"@cluster0.3mite.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-    var dbops = {useUnifiedTopology: true,useNewUrlParser: true}
+    var dbops = {useUnifiedTopology: true,useNewUrlParser: true, useFindAndModify:true}
     mongoose.connect(connectionString,dbops);
     var db = mongoose.connection;
     db.on('connected', function() {
