17cc27b00f68672c35eb56728f42506acc9b5ab2
Sai Sree Kanakati <saisreekanakati@gmail.com>
2021-05-19 09:51:44 +0530

    crud

--- backend/db/dbconnect.js
+++ backend/db/dbconnect.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 module.exports.connect=function(){
     var password = process.env.Mongo_atlas_password;
-    var connectionString = "mongodb+srv://saisreekanakati:"+password+"@cluster0.3mite.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    var connectionString = "mongodb+srv://saisreekanakati:kanna1707@cluster0.3mite.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(connectionString, {});
 mongoose.connection.on('connected', function(){
